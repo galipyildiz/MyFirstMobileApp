cf003d3e14260a4b381c547e7876ca3a97c92dd2
galipyildiz <galipyildiz9606@outlook.com>
2024-03-15 06:36:01 +0300

    Fix: component layout

---
+++ components/RedBox/RedBox.style.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  box: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'red',
+  },
+});
---
+++ components/RedBox/RedBox.tsx
@@ -0,0 +1,8 @@
+import {View} from 'react-native';
+import RedBoxStyle from './RedBox.style';
+
+const RedBox = () => {
+  return <View style={RedBoxStyle.box} />;
+};
+
+export default RedBox;
